d4f27db4e95f5d68f51b7224242955cacc3e2150
defuera <justdevelopers@gmail.com>
2021-06-18 15:30:52 +0200

    home screen

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Wow!'),
+          onPressed: () {
+            print('Wow indeed');
+          },
+        ),
+      ),
+    );
+  }
+}
